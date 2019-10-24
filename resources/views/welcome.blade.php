381fee5feca328f598d9ee8aa75e7d8b5274a2f5
spriteb0b <53108972+spriteb0b@users.noreply.github.com>
2019-10-24 23:24:07 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,24 +17,23 @@
                                             <div class="col-xs-b5"></div>
                                         </div>
                                         <div data-swiper-parallax-x="-500">
-                                            <h1 class="h1 light"> Canon EOS 6D Mark II Kit 24-70mm f4 </h1>
-
+                                            <h1 class="h1 light">KUY OAT RIDER</h1>
                                             <div class="title-underline light left"><span></span></div>
                                         </div>
                                         <div data-swiper-parallax-x="-400">
                                             <div class="simple-article size-4 light transparent">
-                                                <p> Detail </p>
+                                                <p>In feugiat molestie tortor a malesuada. Etiam a venenatis ipsum. Proin pharetra elit at feugiat commodo vel placerat tincidunt sapien nec</p>
                                                 <ul>
-                                                    <li>-26.2 megapixel full-frame CMOS image sensor</li>
-                                                    <li>-45-point cross-type autofocus system</li>
-                                                    <li>-ISO speed 100 - 40000 expandable to 102400</li>
+                                                    <li>20.000h of high quality music</li>
+                                                    <li>Perfect insulation</li>
+                                                    <li>5 years guaranteed work</li>
                                                 </ul>
                                             </div>
                                             <div class="col-xs-b30"></div>
                                         </div>
                                         <div data-swiper-parallax-x="-300">
                                             <div class="buttons-wrapper">
-                                                <div class="simple-article size-5 light transparent">BEST PRICE: ฿99,900</div>
+                                                <div class="simple-article size-5 light transparent">BEST PRICE: $195.00</div>
                                                 <a class="button size-2 style-1" href="#">
                                                     <span class="button-wrapper">
                                                         <span class="icon"><img src="img/icon-1.png" alt=""></span>
@@ -54,8 +53,7 @@
                                 </div>
                             </div>
                             <div class="slider-product-preview" data-swiper-parallax-x="-600">
-                                <img src="img/C2.png" alt="" />
-
+                                <img src="img/C1.png" alt="" />
                             </div>
                             <div class="empty-space col-xs-b80 col-sm-b0"></div>
                         </div>
@@ -423,7 +421,6 @@
                     </div>
                     <div class="swiper-slide">
                         <div class="product-shortcode style-1 big">
-                            <div class="product-label red">20% discount</div>
                             <div class="preview">
                                 <img src="img/product-63.jpg" alt="">
                                 <div class="preview-buttons valign-middle">
