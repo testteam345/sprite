28766c106f6746cf198e7ff89d3def83472155ca
spriteb0b <53108972+spriteb0b@users.noreply.github.com>
2019-11-08 16:24:49 +0700

    footer

--- resources/views/layouts/fontend.blade.php
+++ resources/views/layouts/fontend.blade.php
@@ -606,11 +606,11 @@
                             </div>
                             <div class="empty-space col-xs-b20"></div>
                             <div class="footer-contact"><i class="fa fa-mobile" aria-hidden="true"></i> contact us: <a
-                                    href="tel:+35235551238745">+3 (523) 555 123 8745</a></div>
+                                    href="tel:+35235551238745">09x xxxxxxx</a></div>
                             <div class="footer-contact"><i class="fa fa-envelope-o" aria-hidden="true"></i> email: <a
-                                    href="mailto:office@exzo.com">office@exzo.com</a></div>
+                                    href="mailto:office@exzo.com">OPcamera@gmail.com</a></div>
                             <div class="footer-contact"><i class="fa fa-map-marker" aria-hidden="true"></i> address: <a
-                                    href="#">1st, new york, usa</a></div>
+                                    href="#">1st, Thailand,Bangkok</a></div>
                         </div>
                         <div class="col-sm-6 col-md-3 col-xs-b30 col-md-b0">
                             <h6 class="h6 light">queck links</h6>
@@ -621,19 +621,14 @@
                                         <a href="#">home</a>
                                         <a href="#">about us</a>
                                         <a href="#">products</a>
-                                        <a href="#">services</a>
-                                        <a href="#">blog</a>
-                                        <a href="#">gallery</a>
+                                   
                                         <a href="#">contact</a>
                                     </div>
                                     <div class="col-xs-6">
-                                        <a href="#">privacy policy</a>
-                                        <a href="#">warranty</a>
+                                       
                                         <a href="#">login</a>
                                         <a href="#">registration</a>
-                                        <a href="#">delivery</a>
-                                        <a href="#">pages</a>
-                                        <a href="#">our stores</a>
+                                    
                                     </div>
                                 </div>
                             </div>
@@ -667,31 +662,13 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-6 col-md-3">
-                            <h6 class="h6 light">popular tags</h6>
-                            <div class="empty-space col-xs-b20"></div>
-                            <div class="tags clearfix">
-                                <a class="tag">headphoness</a>
-                                <a class="tag">accessories</a>
-                                <a class="tag">new</a>
-                                <a class="tag">wireless</a>
-                                <a class="tag">cables</a>
-                                <a class="tag">devices</a>
-                                <a class="tag">gadgets</a>
-                                <a class="tag">brands</a>
-                                <a class="tag">replacements</a>
-                                <a class="tag">cases</a>
-                                <a class="tag">cables</a>
-                                <a class="tag">professional</a>
-                            </div>
-                        </div>
-                    </div>
+                       
                 </div>
                 <div class="footer-bottom">
                     <div class="row">
                         <div class="col-lg-8 col-xs-text-center col-lg-text-left col-xs-b20 col-lg-b0">
-                            <div class="copyright">&copy; 2015 All rights reserved. Development by <a
-                                    href="http://themeforest.net/user/unionagency" target="_blank">UnionAgency</a></div>
+                            <div class="copyright">&copy; Copyright by oat&sprite <a
+                             </div>
                             <div class="follow">
                                 <a class="entry" href="#"><i class="fa fa-facebook"></i></a>
                                 <a class="entry" href="#"><i class="fa fa-twitter"></i></a>
@@ -700,20 +677,7 @@
                                 <a class="entry" href="#"><i class="fa fa-pinterest-p"></i></a>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-xs-text-center col-lg-text-right">
-                            <div class="footer-payment-icons">
-                                <a class="entry"><img src="{{ env('APP_ASSET') }}assets/img/thumbnail-4.jpg"
-                                        alt="" /></a>
-                                <a class="entry"><img src="{{ env('APP_ASSET') }}assets/img/thumbnail-5.jpg"
-                                        alt="" /></a>
-                                <a class="entry"><img src="{{ env('APP_ASSET') }}assets/img/thumbnail-6.jpg"
-                                        alt="" /></a>
-                                <a class="entry"><img src="{{ env('APP_ASSET') }}assets/img/thumbnail-7.jpg"
-                                        alt="" /></a>
-                                <a class="entry"><img src="{{ env('APP_ASSET') }}assets/img/thumbnail-8.jpg"
-                                        alt="" /></a>
-                                <a class="entry"><img src="{{ env('APP_ASSET') }}assets/img/thumbnail-9.jpg"
-                                        alt="" /></a>
+                       
                             </div>
                         </div>
                     </div>
